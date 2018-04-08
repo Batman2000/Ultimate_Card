74c3d151cb088b3932e158ec7b666dfd61c08cb8
Batman2000 <nurmhit@mail.ru>
2018-04-09 00:16:40 +0300

    added_more_tests

--- tests.h
+++ tests.h
@@ -16,4 +16,30 @@ TEST(Medical_and_General, testik)
 delete a;
 }
 
+TEST(Medical_and_Bank, testik)
+{
+    Application *a = new Application;
+    EXPECT_EQ("insurance number: 2; blood type: AB", a->get_medical_information());
+    EXPECT_EQ("4242", a->get_bank_information());
+    delete a;
+}
+
+TEST(General_and_Bank, testik)
+{
+    Application *a = new Application;
+    EXPECT_EQ("Vasya Ivanov 1.1.2000", a->get_general_information());
+    EXPECT_EQ("4242", a->get_bank_information());
+    delete a;
+}
+
+TEST(General_and_Bank_and_Medical, testik)
+{
+    Application *a = new Application;
+    EXPECT_EQ("Vasya Ivanov 1.1.2000", a->get_general_information());
+    EXPECT_EQ("4242", a->get_bank_information());
+    EXPECT_EQ("insurance number: 2; blood type: AB", a->get_medical_information());
+    delete a;
+}
+
+
 #endif //ULTIMATE_CARD_TESTS_H
